07615ad2f151d016b192d8a55a7735986c9e9dbb
Kemp <32265362+kempspo@users.noreply.github.com>
2020-05-14 15:23:34 +0800

    Remove unused shell

--- jupyter/before_notebook.sh
+++ jupyter/before_notebook.sh
@@ -5,6 +5,3 @@ export NB_USER=$(echo $NB_USER)
 
 # Give nb user docker permissions
 sudo usermod -aG docker $NB_USER
-
-jupyter notebook --generate-config -y
-echo 'c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"' >> ~/.jupyter/jupyter_notebook_config.py
