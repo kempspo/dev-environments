ccb87069270b5383429b1c2af53dd89503aa5b9f
Kemp <32265362+kempspo@users.noreply.github.com>
2020-05-17 18:31:44 +0800

    Update before_notebook.sh

--- jupyter/before_notebook.sh
+++ jupyter/before_notebook.sh
@@ -3,5 +3,10 @@
 # Export NB_USER
 export NB_USER=$(echo $NB_USER)
 
+jupyter notebook --generate-config -y
+echo 'c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"' >> ~/.jupyter/jupyter_notebook_config.py
+
 # Give nb user docker permissions
 sudo usermod -aG docker $NB_USER
+
+cd /home
