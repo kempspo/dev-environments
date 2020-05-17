a46fa6fd5540c5a2e8381fa73e94368f521eb2cc
Kemp <32265362+kempspo@users.noreply.github.com>
2020-05-18 00:13:31 +0800

    try fixing nginx

--- jupyter/start_notebook.sh
+++ jupyter/start_notebook.sh
@@ -5,3 +5,37 @@ export NB_USER=$(echo $NB_USER)
 
 # change default shell
 export SHELL=`which zsh`
+
+# Add nginx config and start nginx
+sudo rm -f /etc/nginx/sites-enabled/default
+echo "
+map \$http_upgrade \$connection_upgrade {
+        default upgrade;
+        '' close;
+    }
+server {
+    location ~ /api/kernels/ {
+            proxy_pass            http://notebook;
+            proxy_set_header      Host \$host;
+            # websocket support
+            proxy_http_version    1.1;
+            # proxy_set_header      Upgrade "websocket";
+            # proxy_set_header      Connection "Upgrade";
+            proxy_set_header      Upgrade \$http_upgrade;
+            proxy_set_header      Connection \$connection_upgrade;
+            proxy_read_timeout    86400;
+        }
+    location ~ /terminals/ {
+            proxy_pass            http://notebook;
+            proxy_set_header      Host \$host;
+            # websocket support
+            proxy_http_version    1.1;
+            # proxy_set_header      Upgrade "websocket";
+            # proxy_set_header      Connection "Upgrade";
+            proxy_set_header      Upgrade \$http_upgrade;
+            proxy_set_header      Connection \$connection_upgrade;
+            proxy_read_timeout    86400;
+    }    
+}
+" | sudo tee /etc/nginx/sites-enabled/default
+sudo service nginx start
