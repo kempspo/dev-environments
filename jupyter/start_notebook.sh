7a695177288126e0e3f7a965f07033bbc61ff673
Kemp <32265362+kempspo@users.noreply.github.com>
2020-05-18 01:44:02 +0800

    Update start_notebook.sh

--- jupyter/start_notebook.sh
+++ jupyter/start_notebook.sh
@@ -13,18 +13,19 @@ map \$http_upgrade \$connection_upgrade {
         default upgrade;
         '' close;
     }
+upstream notebook {
+    server localhost:8888;
+}
 server {
-    location ~ /api/kernels/ {
-            proxy_pass            http://notebook;
-            proxy_set_header      Host \$host;
-            # websocket support
-            proxy_http_version    1.1;
-            # proxy_set_header      Upgrade "websocket";
-            # proxy_set_header      Connection "Upgrade";
-            proxy_set_header      Upgrade \$http_upgrade;
-            proxy_set_header      Connection \$connection_upgrade;
-            proxy_read_timeout    86400;
-        }
+    location /api/kernels/ {
+        proxy_pass            https://notebook;
+        proxy_set_header      Host $host;
+        # websocket support
+        proxy_http_version    1.1;
+        proxy_set_header      Upgrade "websocket";
+        proxy_set_header      Connection "Upgrade";
+        proxy_read_timeout    86400;
+    }
     location ~ /terminals/ {
             proxy_pass            http://notebook;
             proxy_set_header      Host \$host;
