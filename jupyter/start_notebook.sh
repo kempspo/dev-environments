c3e701a64770bde7b980025c8ac80b294a1430f2
Kemp <32265362+kempspo@users.noreply.github.com>
2020-05-18 22:27:48 +0800

    Update start_notebook.sh

--- jupyter/start_notebook.sh
+++ jupyter/start_notebook.sh
@@ -17,22 +17,35 @@ upstream notebook {
     server http://127.0.0.1:8888;
 }
 server {
+    listen 8888;
+    listen [::]:8888;
+    rewrite ^${JUPYTERHUB_SERVICE_PREFIX:0:(-1)}$ $HUB_HOST$JUPYTERHUB_SERVICE_PREFIX permanent;
+    location $JUPYTERHUB_SERVICE_PREFIX {
+        proxy_pass http://127.0.0.1:8080/;
+        proxy_redirect http://127.0.0.1:8080/ $HUB_HOST$JUPYTERHUB_SERVICE_PREFIX;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection \$connection_upgrade;
+        proxy_read_timeout 20d;
+        proxy_set_header Host \$host;
+        proxy_set_header Accept-Encoding gzip;
+    }
     location ~* (/user/[^/]*)/api/kernels/ {
-        proxy_pass            https://jupyter;
-        proxy_set_header      Host $http_host;
+        proxy_pass            http://127.0.0.1:8080/;
+        proxy_set_header Host \$host;
         # websocket support
         proxy_http_version    1.1;
-        proxy_set_header      Upgrade "websocket";
-        proxy_set_header      Connection "Upgrade";
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection \$connection_upgrade;
         proxy_read_timeout    86400;
     }
     location ~* (/user/[^/]*)/terminals/ {
-        proxy_pass            http://jupyter;
-        proxy_set_header      Host $http_host;
+        proxy_pass            http://127.0.0.1:8080/;
+        proxy_set_header Host \$host;
         # websocket support
         proxy_http_version    1.1;
-        proxy_set_header      Upgrade "websocket";
-        proxy_set_header      Connection "Upgrade";
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection \$connection_upgrade;
         proxy_read_timeout    86400;
     }    
 }
