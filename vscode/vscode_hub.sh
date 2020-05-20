b0053f4ee62e7b60c24bed79efa5689db22d649d
Kemp <32265362+kempspo@users.noreply.github.com>
2020-05-21 01:44:36 +0800

    i hate my life

--- vscode/vscode_hub.sh
+++ vscode/vscode_hub.sh
@@ -11,8 +11,8 @@ map \$http_upgrade \$connection_upgrade {
         '' close;
     }
 server {
-        listen 8080;
-        listen [::]:8080;
+        listen 4180;
+        listen [::]:4180;
         location / {
              proxy_pass http://127.0.0.1:8080/;
              proxy_http_version 1.1;
