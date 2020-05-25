39e53585a798b52f615b890742a9659d7f160a5b
Kemp <32265362+kempspo@users.noreply.github.com>
2020-05-25 22:22:38 +0800

    Add conda envs

--- jupyter/before_notebook.sh
+++ jupyter/before_notebook.sh
@@ -3,6 +3,12 @@
 # Export NB_USER
 export NB_USER=$(echo $NB_USER)
 
+# replace conda envs folder with persistent envs folder
+rm -rf /opt/conda/envs
+mkdir -p /mnt/envs
+ln -s /mnt/envs /opt/conda/envs
+sudo chown -R 1000:100 /opt/conda/envs
+
 jupyter notebook --generate-config -y
 echo 'c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"' >> ~/.jupyter/jupyter_notebook_config.py
 
